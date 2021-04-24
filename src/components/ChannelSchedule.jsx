4e22225ee2ab60c946eef352c2218800674d1c02
Hitomi <hitomi.winberg@gmail.com>
2021-04-24 13:10:57 +0200

    Added Card to ChannesSchedule.jsx

--- src/components/ChannelSchedule.jsx
+++ src/components/ChannelSchedule.jsx
@@ -1,11 +1,41 @@
 import { useContext, useEffect, useState} from "react";
 import { ChannelContext } from "../contexts/ChannelContext"
-
+import { Card, Button, Container, Col, Row } from "react-bootstrap";
+import styles from "../css/ChannelSchedule.module.css"
 const ChannelSchedule=()=>{
   const { schedule }=useContext(ChannelContext);
-  console.log(schedule)
+  console.log(schedule.map((s)=>(s.starttimeutc.slice(11,16))));
+  const renderSchedule=()=>{
+    return (
+      schedule.map((episode,i)=>(
+       <Col key={i} xs={12}>
+       <Card  >
+       <Row>
+         <Col xs={2}>
+         <div className={styles.time}>{episode.starttimeutc.slice(11,16)}</div></Col>
+        <Col xs={2} style={{padding:"1.25rem"}}>
+          <Card.Img src={episode.imageurl} alt={"episode image"}/>
+        </Col>
+        <Col xs={7} >
+        <Card.Body>
+          <Card.Title>{episode.title}</Card.Title>
+          <Card.Text>{episode.description}</Card.Text>
+        </Card.Body>
+        </Col>
+        </Row>
+      </Card>
+       </Col>
+      ))
+    )
+  } 
   return(
-    <div>{schedule.map((episode)=>(<p>{episode.title}</p>))}</div>
+    <div className={styles.schedule}>
+      <Container >
+      <Row>      
+      {schedule && renderSchedule()}
+      </Row> 
+      </Container>
+    </div>
   )
 }
 
