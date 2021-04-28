19d8c6ad8202a46b7607d085415ca1ea177e7b31
Hitomi <hitomi.winberg@gmail.com>
2021-04-28 09:59:33 +0200

    Added Heart to programsByCategories

--- src/pages/ProgramsByCategory.jsx
+++ src/pages/ProgramsByCategory.jsx
@@ -2,8 +2,10 @@ import { useContext, useEffect} from "react"
 import { useHistory } from "react-router-dom"
 import { CategoryContext } from "../contexts/CategoryContext"
 import { ProgramContext } from "../contexts/ProgramContext"
+import { FavoriteContext } from "../contexts/FavoriteContext";
+import { UserContext } from "../contexts/UserContext";
 import { Card, Container, Col, Row } from "react-bootstrap";
-import { Tag } from 'react-bootstrap-icons';
+import { Tag, Heart } from 'react-bootstrap-icons';
 import styles from "../css/ProgramsByCategory.module.css"
 
 
@@ -11,6 +13,8 @@ const ProgramsByCategory = (props) => {
   const { category, getCategoryById } = useContext( CategoryContext )
   const {programs , getProgramsByCategory } = useContext( ProgramContext )
   const {categoryId} = props.match.params;
+  const { user } = useContext ( UserContext );
+  const { addProgramToFavorites } = useContext( FavoriteContext );
   const history = useHistory();
   useEffect(() => {
     getCategoryById(categoryId)
@@ -21,6 +25,20 @@ const ProgramsByCategory = (props) => {
   const handleClick=(programId)=>{
     history.push(`/programs/allprogram/${programId}`)
   }  
+  const handleOnClickHeart= async (e, programId, channelId)=>{
+
+    e.stopPropagation()
+    let favoriteProgram ={
+      programId,
+    }
+    let result = await  addProgramToFavorites(favoriteProgram, channelId);
+    if (result.success ) {
+      console.log(result.success)
+    } else {
+      console.log(result.error)
+    }
+  };
+
   const renderMenuBar = () => {
     return (
       <ul className={styles.menuList}>
@@ -28,24 +46,28 @@ const ProgramsByCategory = (props) => {
       </ul>
     );
   };
+
   const renderPrograms=()=>{
     return (
       programs.map((program)=>(
        <Col key={program.id} xs={12} md={12} lg={6}   onClick={() => handleClick(program.id) }>
        <Card className={styles.card} >
        <Row>
-        <Col xs={3} md={3} lg={3} style={{padding:"1.25rem"}}>
+        <Col xs={3}  style={{padding:"1.25rem"}}>
           <Card.Img src={program.programimagewide} alt={"program image"}/>
         </Col>
-        <Col xs={8} md={8} lg={8}>
+        <Col xs={7} >
         <Card.Body>
           <Card.Title>{program.name}</Card.Title>
           <Card.Text>
-          <Tag color="gray" size={25} />
-            
+          <Tag color="gray" size={25} />           
             {program.programcategory["name"]}</Card.Text>
         </Card.Body>
         </Col>
+        {user &&
+       <Col  xs={1}  style={{paddingTop:"1.25rem"}} >
+           <Heart color="gray" size={25}  onClick={(e)=>{handleOnClickHeart(e,program.id, props.channelId)}}/ > 
+        </Col> }    
         </Row>
       </Card>
        </Col>
